f541634e2b51389eae4a9f0b851e10178f836837
KoliaLev <kolialevkovich@gmail.com>
2021-04-27 10:08:39 +0300

    setting for deployment

--- app.js
+++ app.js
@@ -9,6 +9,14 @@ app.use(express.json({ extendet: true })); // мидлвэер для парси
 app.use("/api/auth", require("./routes/auth.routes"));
 app.use("/api/create", require("./routes/spend.routes"));
 
+if (process.env.NODE_ENV === "prodaction") {
+  app.use("/", express.static(path.join(__dirname, "client", "build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const PORT = config.get("port") || 5000;
 
 async function start() {
